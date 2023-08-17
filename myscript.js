181740db714e393fc89552023c81ecf57502a0bb
Banele Ndaba <Banele.Ndaba@OMNIA.CO.ZA>
2023-08-17 14:23:50 +0200

    App Fixes

--- myscript.js
+++ myscript.js
@@ -1,60 +1,60 @@
 var questions = [{
-    questions: "What is the baby of a Moth known as?",
+    question: "What is the baby of a Moth known as?",
     choices: ["baby", "infant", "kit", "larva"],
     correctAnswer: 3
 
 }, {
-    questions: "What is the adult of a kid called?",
+    question: "What is the adult of a kid called?",
     choices: ["calf", "doe", "goat", "chick"],
     correctAnswer: 2
 
 }, {
-    questions: "What is the young of a Bufallo called?",
+    question: "What is the young of a Bufallo called?",
     choices: ["calf", "baby", "pup", "cow"],
     correctAnswer: 0
 
 }, {
-    questions: "What is a baby Aligator called?",
+    question: "What is a baby Aligator called?",
     choices: ["baby", "gator", "hatchling", "calf"],
     correctAnswer: 2
 
 }, {
-    questions: "What is a baby Goose called?",
+    question: "What is a baby Goose called?",
     choices: ["gooser", "gosling", "gup", "pup"],
     correctAnswer: 1
 
 }, {
-    questions: "What is a baby Hamster called?",
+    question: "What is a baby Hamster called?",
     choices: ["pup", "chick", "infant", "billy"],
     correctAnswer: 0
 
 }, {
-    questions: "What is a baby Hawk called?",
+    question: "What is a baby Hawk called?",
     choices: ["hawklett", "pup", "larva", "eyas"],
     correctAnswer: 3
 
 }, {
-    questions: "What is a baby grasshopper called?",
+    question: "What is a baby grasshopper called?",
     choices: ["hopper", "nymph", "stick", "pup"],
     correctAnswer: 1
 
 }, {
-    questions: "What is a baby Kangaroo called?",
+    question: "What is a baby Kangaroo called?",
     choices: ["kinga", "joey", "calf", "baby"],
     correctAnswer: 1
 
 }, {
-    questions: "What is a baby Whale called?",
+    question: "What is a baby Whale called?",
     choices: ["whala", "cub", "grup", "infant"],
     correctAnswer: 1
 
 }, {
-    questions: "What is a baby Monkey called?",
+    question: "What is a baby Monkey called?",
     choices: ["infant", "baby", "calf", "pup"],
     correctAnswer: 0
 
 }, {
-    questions: "What is a baby Bear called?",
+    question: "What is a baby Bear called?",
     choices: ["cub", "baby balu", "young bear", "bearlet"],
     correctAnswer: 0
 
@@ -65,7 +65,7 @@ var correctAnswers=0;
 var quizOver=false;
 
 $(document).ready(function () {
-    displayCurrentQuestions();
+    displayCurrentQuestion();
     $(this).find(".quizMessage").hide();
     $(this).find(".nextButton").on("click", function () {
         if(!quizOver){
@@ -81,7 +81,7 @@ $(document).ready(function () {
                 }
                 currentQuestion++;
                 if(currentQuestion<questions.length){
-                    displayCurrentQuestions();
+                    displayCurrentQuestion();
                 }
                 else{
                     displayScore();
@@ -93,13 +93,13 @@ $(document).ready(function () {
             quizOver=false;
             $(document).find(".nextButton").text("Next Question");
             resetQuiz();
-            displayCurrentQuestions();
+            displayCurrentQuestion();
             hideScore();
         }
     })
 })
 
-function displayCurrentQuestions() {
+function displayCurrentQuestion() {
     console.log("In display current Question");
 
     var question=questions[currentQuestion].question;
@@ -116,18 +116,17 @@ function displayCurrentQuestions() {
         choice=questions[currentQuestion].choices[i];
         $('<li><input type="radio" value='+i+'name="dynradio" />'+choice+ '<.li>').appendTo(choiceList);
     }
+}
+
 function resetQuiz(){
     currentQuestion=0;
     correctAnswers=0;
     hideScore();
-
+}
 function displayScore(){
-    $(document).find(".quizContainer > .result").text("You scored: "+ correctAnswers + "out of: "+questions.length);
+    $(document).find(".quizContainer > .result").text("You scored: "+ correctAnswers + " out of: "+questions.length);
     $(document).find(".quizContainer > .result").show();
 }
-
 function hideScore(){
     $(document).find(".result").hide();
-}
-}
 }
